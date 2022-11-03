cd6175f917fb79f262a776b55ad753dbc4de9ced
takuya-namekawa <tatatatatata89@gmail.com>
2022-11-03 12:32:00 +0000

    Quiz-app

---
+++ db/migrate/20221103122849_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[6.1]
+  def change
+    create_table :games do |t|
+      
+      t.string :status
+      t.string :result
+
+      t.timestamps
+    end
+  end
+end
