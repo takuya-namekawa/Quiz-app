aec006f7dfa956defd66fcbeaf7920b2ee3d8958
takuya-namekawa <tatatatatata89@gmail.com>
2022-11-04 13:37:02 +0000

    3-4

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -4,4 +4,21 @@ class ProgressesController < ApplicationController
     @progress = Progress.new
     @question = Question.first #ここでは質問データを1件取得しています。
   end
+
+  def create
+    current_game = Game.find(params[:game_id])
+
+    #回答した内容を保存する
+    progress = current_game.progresses.new(create_params)
+    progress.assign_sequence
+    progress.save!
+
+    redirect_to new_game_progresses_path(current_game)
+  end
+
+  private
+  
+  def create_params
+    params.require(:progress).permit(:question_id, :answer)
+  end
 end
