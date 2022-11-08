d5ec5286fbfd9136fc2f2a6e5acb3c54083adbb8
takuya-namekawa <tatatatatata89@gmail.com>
2022-11-08 09:14:30 +0000

    4-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "games#new"
-  resources :games, only: %i[new create] do
+  resources :games, only: %i[new create update] do
     member do
       get :give_up
       get :challenge
+      get :correct
     end
     resource :progresses, only: %i[new create]
   end
