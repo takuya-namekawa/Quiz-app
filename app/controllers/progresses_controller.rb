b8871ea237b80b7b5425746d6a2a66250edcfa6a
takuya-namekawa <tatatatatata89@gmail.com>
2022-11-09 04:07:02 +0000

    4-5

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -9,12 +9,21 @@ class ProgressesController < ApplicationController
   def create
     current_game = Game.find(params[:game_id])
 
+# 回答した内容を保存する
     progress = current_game.progresses.new(create_params)
     progress.assign_sequence
     progress.save!
 
+# 絞り込みを実行
     @extract_comics = ExtractionAlgorithm.new(current_game).compute
 
+# 絞り込み結果が0件の場合、ギブアップ画面へ遷移
+    if @extract_comics.count == 0
+      redirect_to give_up_game_path(current_game)
+      return
+    end
+
+# 絞り込み結果が1件の場合、チャレンジ(正解を当てにいく)へ遷移
     if @extract_comics.count == 1
       redirect_to challenge_game_path(current_game)
       return
--- db/seeds.rb
+++ db/seeds.rb
@@ -67,6 +67,17 @@ case ENV['SECTION']  #seedを実行する際にSECTION=○という引数に章�
       { content: '連載終了してますか？', algorithm: 'serialization_end', eval_value: nil },
     ]
 
+  when "4-5"
+    comics = [
+      { title: 'ワンピース ONE PIECE', genre: '少年漫画　海賊　冒険ファンタジー　バトル', serialization_started_year: 1997, serialization_end_year: nil, publisher: '週間少年ジャンプ' },
+      { title: 'ゴルゴ13', genre: 'ハードボイルド　漫画', serialization_started_year: 1968, serialization_end_year: nil, publisher: 'ビックコミック'},
+      ]
+
+    questions = [
+      { content: '連載終了していますか？', algorithm: 'serialization_end', eval_value: nil },
+      { content: '連載は週間少年ジャンプ?', algorithm: 'publisher_match', eval_value: '週間少年ジャンプ' },
+      ]
+
 end
 
 if comics.present?  #present?は、Rubyの真偽判定の1つで、オブジェクトに値が格納されているかを評価するときに使用します。
